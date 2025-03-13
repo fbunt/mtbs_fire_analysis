f0761a3ca83bc136963417260131eeb074b9b2bf
Fred Bunt <fredrick.bunt@gmail.com>
2025-03-13 17:44:25 -0600

    Add support for Western US

--- 00_rasters_preprocess.py
+++ 00_rasters_preprocess.py
@@ -3,11 +3,12 @@ import glob
 from pathlib import Path
 
 import dask.array as da
+import geopandas as gpd
 import numpy as np
 from dask.diagnostics import ProgressBar
 
 import raster_tools as rts
-from paths import CLEANED_RASTER_DATA_DIR, RAW_RASTER_DATA_DIR
+from paths import CLEANED_RASTER_DATA_DIR, RAW_RASTER_DATA_DIR, STATES_PATH
 
 DATA_TIF_FMT = "mtbs_{aoi}_{year}.tif"
 
@@ -20,19 +21,31 @@ def _get_parser():
     return p
 
 
-def get_data_raster_path(year, aoi_code):
-    return (
-        RAW_RASTER_DATA_DIR
-        / f"{year}"
-        / DATA_TIF_FMT.format(aoi=aoi_code, year=year)
-    )
+def get_aoi_geom(aoi_code, crs):
+    geoms = gpd.read_file(STATES_PATH)
+    if aoi_code in ("ALL", "CONUS"):
+        geoms = geoms[geoms.GEOID.astype(int) < 60]
+        if aoi_code == "CONUS":
+            geoms = geoms[~geoms.STUSPS.isin(("AK", "HI"))]
+    else:
+        geoms = geoms[aoi_code == geoms.STUSPS]
+    return gpd.GeoSeries([geoms.geometry.to_crs(crs).union_all()], crs=crs)
 
 
 def main(aoi_code):
+    is_wus = aoi_code == "WUS"
+    if is_wus:
+        aoi_code = "CONUS"
     raster_paths = sorted(
         glob.glob(f"{RAW_RASTER_DATA_DIR}/**/*{aoi_code}*.tif")
     )[:-2]
     rasters = [rts.Raster(p) for p in raster_paths]
+    if is_wus:
+        crs = rasters[0].crs
+        # Buffer out 50 km
+        gs = get_aoi_geom("WUS", crs).buffer(50_000)
+        bounds = gs.total_bounds
+        rasters = [rts.clipping.clip_box(r, bounds) for r in rasters]
     combined = rts.band_concat(rasters, join="outer")
     data = da.ones((1, *combined.shape[1:]), dtype=np.int8)
     combined = rts.data_to_raster_like(data, combined)
@@ -41,7 +54,13 @@ def main(aoi_code):
     rasters_rp = [r.reproject(geobox) for r in rasters]
     for data_path, r in zip(raster_paths, rasters_rp):
         data_path = Path(data_path)
-        out_path = CLEANED_RASTER_DATA_DIR / data_path.name
+        if is_wus:
+            out_path = (
+                CLEANED_RASTER_DATA_DIR
+                / f"mtbs_WUS_{data_path.name[-8:-4]}.tif"
+            )
+        else:
+            out_path = CLEANED_RASTER_DATA_DIR / data_path.name
         print(out_path)
         if out_path.exists():
             print("Skipping")
