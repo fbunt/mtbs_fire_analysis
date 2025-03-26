4d55fd1603a4950b29328f503fa5054673204c5c
Fred Bunt <fredrick.bunt@gmail.com>
2025-03-26 15:50:12 -0600

    Move default grid and crs to defaults.py

--- utils.py
+++ utils.py
@@ -3,54 +3,13 @@ from functools import partial
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-import rasterio as rio
 from affine import Affine
 
-DEFAULT_GEOHASH_AFFINE = Affine(
-    30.0, 0.0, -2406135.0, 0.0, -30.0, 3222585.0, 0.0, 0.0, 1.0
+from defaults import (
+    DEFAULT_GEOHASH_AFFINE,
+    DEFAULT_GEOHASH_GRID_SHAPE,
+    DEFAULT_CRS,
 )
-DEFAULT_GEOHASH_GRID_SHAPE = (100150, 157144)
-DEFAULT_CRS = rio.CRS.from_wkt(
-"""
-PROJCRS["USA_Contiguous_Albers_Equal_Area_Conic_USGS_version",
-    BASEGEOGCRS["NAD83",
-        DATUM["North American Datum 1983",
-            ELLIPSOID["GRS 1980",6378137,298.257222101004,
-                LENGTHUNIT["metre",1]]],
-        PRIMEM["Greenwich",0,
-            ANGLEUNIT["degree",0.0174532925199433]],
-        ID["EPSG",4269]],
-    CONVERSION["unnamed",
-        METHOD["Albers Equal Area",
-            ID["EPSG",9822]],
-        PARAMETER["Latitude of false origin",23,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8821]],
-        PARAMETER["Longitude of false origin",-96,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8822]],
-        PARAMETER["Latitude of 1st standard parallel",29.5,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8823]],
-        PARAMETER["Latitude of 2nd standard parallel",45.5,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8824]],
-        PARAMETER["Easting at false origin",0,
-            LENGTHUNIT["metre",1],
-            ID["EPSG",8826]],
-        PARAMETER["Northing at false origin",0,
-            LENGTHUNIT["metre",1],
-            ID["EPSG",8827]]],
-    CS[Cartesian,2],
-        AXIS["easting",east,
-            ORDER[1],
-            LENGTHUNIT["metre",1,
-                ID["EPSG",9001]]],
-        AXIS["northing",north,
-            ORDER[2],
-            LENGTHUNIT["metre",1,
-                ID["EPSG",9001]]]]
-""")
 
 
 class GridGeohasher:
@@ -115,4 +74,3 @@ def add_geometry_from_geohash(ddf, hasher=None):
     meta = ddf._meta.copy()
     meta = gpd.GeoDataFrame(meta, geometry=gpd.GeoSeries([], crs=hasher.crs))
     return ddf.map_partitions(func, meta=meta)
-
