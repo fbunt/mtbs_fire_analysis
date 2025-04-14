a4f506d329ca34a313818bc1899276239a05dea7
Fred Bunt <fredrick.bunt@gmail.com>
2025-04-14 14:12:38 -0600

    Add NLCD and update paths to new layout

--- paths.py
+++ paths.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 
 MTBS_ROOT = Path("/var/mnt/fastdata02/mtbs")
-RAW_RASTER_DATA_DIR = MTBS_ROOT / "MTBS_BSmosaics"
-CLEANED_RASTER_DATA_DIR = MTBS_ROOT / "MTBS_BSmosaics_cleaned"
+MTBS_RASTER_DIR = MTBS_ROOT / "mtbs_bs_rasters"
+RAW_RASTER_DATA_DIR = MTBS_RASTER_DIR / "raw"
+CLEANED_RASTER_DATA_DIR = MTBS_RASTER_DIR / "cleaned"
 ROOT_TMP_DIR = Path("/var/mnt/fastdata01/fire_tmp/")
 RESULTS_DIR = Path("/var/mnt/fastdata02/mtbs/results")
 
-RAW_PERIMS_PATH = MTBS_ROOT / "mtbs_perims" / "mtbs_perims_DD.shp"
-PERIMS_PATH = MTBS_ROOT / "mtbs_perims" / "mtbs_perims_trimmed.pqt"
-RAW_STATES_PATH = MTBS_ROOT / "state_borders" / "cb_2018_us_state_5m.shp"
-STATES_PATH = MTBS_ROOT / "state_borders" / "states.shp"
-RAW_ECO_REGIONS_PATH = MTBS_ROOT / "eco_regions" / "NA_CEC_Eco_Level3.shp"
-ECO_REGIONS_PATH = MTBS_ROOT / "eco_regions" / "eco_regions.shp"
+PERIMS_DIR = MTBS_ROOT / "mtbs_perims"
+RAW_PERIMS_PATH = PERIMS_DIR / "raw" / "mtbs_perims_DD.shp"
+PERIMS_PATH = PERIMS_DIR / "cleaned" / "mtbs_perims_trimmed.pqt"
+STATES_DIR = MTBS_ROOT / "state_borders"
+RAW_STATES_PATH = STATES_DIR / "raw" / "cb_2018_us_state_5m.shp"
+STATES_PATH = STATES_DIR / "cleaned" / "states.shp"
+ECO_REGIONS_DIR = MTBS_ROOT / "eco_regions"
+RAW_ECO_REGIONS_PATH = ECO_REGIONS_DIR / "raw" / "NA_CEC_Eco_Level3.shp"
+ECO_REGIONS_PATH = ECO_REGIONS_DIR / "cleaned" / "eco_regions.shp"
+NLCD_DIR = MTBS_ROOT / "nlcd"
+RAW_NLCD = NLCD_DIR / "raw"
+NLCD_PATH = NLCD_DIR / "cleaned"
