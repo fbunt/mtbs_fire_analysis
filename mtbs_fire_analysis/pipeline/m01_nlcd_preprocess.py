1e45f2549bcd36afea59ca94219badb0997d69fe
Fred Bunt <fredrick.bunt@gmail.com>
2025-06-10 19:09:29 -0600

    refactor to use bootstrapped grid

--- mtbs_fire_analysis/pipeline/m01_nlcd_preprocess.py
+++ mtbs_fire_analysis/pipeline/m01_nlcd_preprocess.py
@@ -1,30 +1,19 @@
-import argparse
 import glob
 from pathlib import Path
 
 import raster_tools as rts
 from dask.diagnostics import ProgressBar
 
+from mtbs_fire_analysis.defaults import DEFAULT_GEOHASH_GEOBOX
 from mtbs_fire_analysis.pipeline.paths import NLCD_PATH, RAW_NLCD
 
 
-def _path(path):
-    path = Path(path)
-    assert path.exists()
-    return path
-
-
-def _get_parser():
-    p = argparse.ArgumentParser()
-    p.add_argument("like_path", type=_path, help="Path to like raster")
-    return p
-
-
-def main(like_path):
-    like = rts.Raster(like_path)
+def main():
     nlcd_paths = [Path(p) for p in sorted(glob.glob(str(RAW_NLCD / "*.tif")))]
-    nlcd_rasters = [rts.Raster(p).reproject(like.geobox) for p in nlcd_paths]
-    for r, path in zip(nlcd_rasters, nlcd_paths):
+    nlcd_rasters = [
+        rts.Raster(p).reproject(DEFAULT_GEOHASH_GEOBOX) for p in nlcd_paths
+    ]
+    for r, path in zip(nlcd_rasters, nlcd_paths, strict=True):
         print(path)
         outpath = NLCD_PATH / path.name
         with ProgressBar():
@@ -32,5 +21,4 @@ def main(like_path):
 
 
 if __name__ == "__main__":
-    args = _get_parser().parse_args()
-    main(args.like_path)
+    main()
