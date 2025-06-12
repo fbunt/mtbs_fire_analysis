4d766496a42d7eb43cd13d38b58cc9390a349055
Fred Bunt <fredrick.bunt@gmail.com>
2025-06-12 17:35:36 -0600

    Add script for rasterizing the perimeters

---
+++ mtbs_fire_analysis/pipeline/m01_rasterize_perims.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+exe() {
+    echo "\$ $@"
+    "$@"
+}
+
+rasterize() {
+    year=$1
+    src=$(python -c "from mtbs_fire_analysis.pipeline.paths import PERIMS_BY_YEAR_PATH; print(PERIMS_BY_YEAR_PATH)")
+    dst_dir=$(python -c "from mtbs_fire_analysis.pipeline.paths import PERIMS_RASTERS_PATH; print(PERIMS_RASTERS_PATH)")
+    dst="${dst_dir}/${year}.tif"
+
+    if [ ! -f "${dst}" ]; then
+        exe gdal_rasterize -l "${year}" -a days_since_epoch \
+            -ot Int16 -a_nodata -1 -init -1 -of GTiff \
+            -tr 30.0 30.0 -te -2406135.0 218085.0 2308185.0 3222585.0 \
+            -co TILED=YES -co BIGTIFF=YES -co COMPRESS=LZW \
+            "${src}" "${dst}"
+    else
+        echo "Skipping ${year}"
+    fi
+}
+
+
+parallel --tag --verbose --linebuffer --keep-order \
+    rasterize {} ::: $(seq 1984 2022)
