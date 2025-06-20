cbcda7b98dc1b33fee694656c83efc9698b274f9
Fred Bunt <fredrick.bunt@gmail.com>
2025-06-20 16:18:53 -0600

    Refactor to skip if present and copy 1985 to 1984

--- mtbs_fire_analysis/pipeline/m01_nlcd_preprocess.py
+++ mtbs_fire_analysis/pipeline/m01_nlcd_preprocess.py
@@ -1,4 +1,5 @@
 import glob
+import shutil
 from pathlib import Path
 
 import raster_tools as rts
@@ -13,11 +14,22 @@ def main():
     nlcd_rasters = [
         rts.Raster(p).reproject(DEFAULT_GEOHASH_GEOBOX) for p in nlcd_paths
     ]
+    path1984 = None
+    path1985 = None
     for r, path in zip(nlcd_rasters, nlcd_paths, strict=True):
-        print(path)
         outpath = NLCD_PATH / path.name
+        if path1984 is None and "1985" in path.name:
+            path1984 = NLCD_PATH / path.name.replace("1985", "1984")
+            path1985 = outpath
+        print(f"{path} --> {outpath}")
+        if outpath.exists():
+            print("Skipping")
+            continue
         with ProgressBar():
-            r.save(outpath)
+            r.save(outpath, tiled=True)
+    # Duplicate 1985 to get an NLCD for 1984
+    print("Copying 1985 to 1984")
+    shutil.copy(path1985, path1984)
 
 
 if __name__ == "__main__":
