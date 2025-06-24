5871ad17e4179eb07fdfa432f4060587ea32f06c
Fred Bunt <fredrick.bunt@gmail.com>
2025-06-24 12:19:26 -0600

    Cleanup bp script. WIP

--- mtbs_fire_analysis/analysis/calc_burn_probability.py
+++ mtbs_fire_analysis/analysis/calc_burn_probability.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import argparse
 
 import dask.array as da
 import numpy as np
@@ -7,10 +7,15 @@ import raster_tools as rts
 import rioxarray as xrio
 from dask.diagnostics import ProgressBar
 
-DATA_DIR = Path("/run/media/fire_analysis/data")
+from mtbs_fire_analysis.pipeline.paths import (
+    ECO_REGIONS_RASTER_PATH,
+    MTBS_ROOT,
+    ST_PATH,
+    get_nlcd_raster_path,
+)
 
 
-def bp_chunk(st, nlcd, eco, geohash, valid):
+def bp_chunk(st, nlcd, eco, geohash, valid, hazard_table_path):
     shape = st.shape
     st = st[valid]
     nlcd = nlcd[valid]
@@ -31,7 +36,7 @@ def bp_chunk(st, nlcd, eco, geohash, valid):
     eco = None
     geohash = None
 
-    hazard_tbl = pl.read_parquet(DATA_DIR / "hazard_table.parquet")
+    hazard_tbl = pl.read_parquet(hazard_table_path)
     frame = frame.join(hazard_tbl, on=["eco", "nlcd", "st"], how="left").sort(
         "idx"
     )
@@ -45,34 +50,55 @@ def bp_chunk(st, nlcd, eco, geohash, valid):
     return result
 
 
-geohash_path = DATA_DIR / "hash_grid.tif"
-st_path = DATA_DIR / "st/st.tif"
-nlcd_path = DATA_DIR / "nlcd/cleaned/Annual_NLCD_LndCov_2022_CU_C1V0.tif"
-eco_path = DATA_DIR / "eco_retions/eco_regions.tif"
+def main(eco_level, year):
+    geohash_raster = rts.Raster(MTBS_ROOT / "hash_grid.tif")
+    st_path = ST_PATH / "st.tif"
+    nlcd_path = get_nlcd_raster_path(year)
+    eco_path = ECO_REGIONS_RASTER_PATH / f"eco_lvl_{eco_level}.tif"
+    # TODO: Add to paths?
+    hazard_table_path = MTBS_ROOT / "hazard_table.parquet"
 
-# The geohash raster is the largest raster in terms of dtype so use its
-# chunksize.
-geohash_raster = rts.Raster(DATA_DIR / "hash_grid.tif")
-chunks = geohash_raster.data.chunks
-geohash_data = geohash_raster.data
-# Align the chunks of the other rasters to keep rechunking overhead to 0.
-st_data = xrio.open_rasterio(st_path, chunks=chunks).data
-nlcd_data = xrio.open_rasterio(nlcd_path, chunks=chunks).data
-nlcd_nv = rts.Raster(nlcd_path).null_value
-valid_data = ~rts.raster.get_mask_from_data(nlcd_data, nlcd_nv)
-eco_data = xrio.open_rasterio(eco_path, chunks=chunks).data
+    # The geohash raster is the largest raster in terms of dtype so use its
+    # chunksize.
+    chunks = geohash_raster.data.chunks
+    geohash_data = geohash_raster.data
+    # Align the chunks of the other rasters to keep rechunking overhead to 0.
+    st_data = xrio.open_rasterio(st_path, chunks=chunks).data
+    nlcd_data = xrio.open_rasterio(nlcd_path, chunks=chunks).data
+    nlcd_nv = rts.Raster(nlcd_path).null_value
+    valid_data = ~rts.raster.get_mask_from_data(nlcd_data, nlcd_nv)
+    eco_data = xrio.open_rasterio(eco_path, chunks=chunks).data
 
-bp_data = da.map_blocks(
-    bp_chunk,
-    st_data,
-    nlcd_data,
-    eco_data,
-    geohash_data,
-    valid_data,
-    chunks=chunks,
-    dtype="float32",
-    meta=np.array((), dtype="float32"),
-)
-bp_raster = rts.data_to_raster_like(bp_data, geohash_raster, nv=-1)
-with ProgressBar():
-    bp_raster.save(DATA_DIR / "bp.tif")
+    bp_data = da.map_blocks(
+        bp_chunk,
+        st_data,
+        nlcd_data,
+        eco_data,
+        geohash_data,
+        valid_data,
+        chunks=chunks,
+        dtype="float32",
+        meta=np.array((), dtype="float32"),
+        # Func params
+        hazard_table_path=hazard_table_path,
+    )
+    bp_raster = rts.data_to_raster_like(bp_data, geohash_raster, nv=-1)
+    with ProgressBar():
+        bp_raster.save(MTBS_ROOT / f"bp_eco_lvl_{eco_level}_{year}.tif")
+
+
+def _get_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument(
+        "eco_level", type=int, default=1, help="The eco regions level to use."
+    )
+    p.add_argument(
+        "year", type=int, help="The year to calculate burn probability for"
+    )
+    return p
+
+
+if __name__ == "__main__":
+    args = _get_parser().parse_args()
+    assert args.year > 1984
+    main(args.eco_level, args.year)
