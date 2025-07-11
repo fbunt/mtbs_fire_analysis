9b63615de3c79671301f740b76ac461901bcc4f2
Fred Bunt <fredrick.bunt@gmail.com>
2025-07-11 16:46:02 -0600

    Fix path gathering op

--- mtbs_fire_analysis/pipeline/m02_nlcd_mode.py
+++ mtbs_fire_analysis/pipeline/m02_nlcd_mode.py
@@ -5,8 +5,8 @@ import raster_tools as rts
 
 from mtbs_fire_analysis.pipeline.paths import (
     NLCD_MODE_RASTER_PATH,
-    NLCD_PATH,
     NLCD_STACK_VRT_PATH,
+    get_nlcd_raster_path,
 )
 from mtbs_fire_analysis.utils import (
     protected_raster_save_with_cleanup,
@@ -15,7 +15,7 @@ from mtbs_fire_analysis.utils import (
 
 
 def main():
-    nlcd_paths = [Path(p) for p in sorted(glob.glob(str(NLCD_PATH / "*.tif")))]
+    nlcd_paths = [get_nlcd_raster_path(y) for y in range(1984, 2022)]
     stack_rasters_as_vrt(nlcd_paths, NLCD_STACK_VRT_PATH)
     nlcd_stack = rts.Raster(NLCD_STACK_VRT_PATH)
     mode = rts.general.local_stats(nlcd_stack, "mode")
